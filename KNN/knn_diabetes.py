139fb9337f9604e0fbb47be48a612172bb6a7659
Mubashir Ali <mubaxhiralley@gmail.com>
2026-09-13 22:10:18 +0500

    Add KNN imports

---
+++ KNN/knn_diabetes.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+from sklearn.model_selection import train_test_split, cross_val_score
+from sklearn.preprocessing import StandardScaler
+from sklearn.impute import SimpleImputer
+from sklearn.neighbors import KNeighborsClassifier
+
+from sklearn.metrics import (
+    accuracy_score,
+    confusion_matrix,
+    classification_report,
+    roc_auc_score
+)
