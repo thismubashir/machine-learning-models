0ed285c1f326b107df3e0fec830158ebf64d5615
Mubashir Ali <mubaxhiralley@gmail.com>
2026-09-16 20:55:58 +0500

    Add decision tree train test split

--- Decision-Tree/decision_tree_heart_disease.py
+++ Decision-Tree/decision_tree_heart_disease.py
@@ -55,3 +55,21 @@ df = pd.get_dummies(
 X = df.drop("target", axis=1)
 
 y = df["target"]
+X_train, X_test, y_train, y_test = train_test_split(
+
+    X,
+
+    y,
+
+    test_size=0.2,
+
+    random_state=42,
+
+    stratify=y
+
+)
+
+
+X_train = X_train.fillna(X_train.median())
+
+X_test = X_test.fillna(X_train.median())
