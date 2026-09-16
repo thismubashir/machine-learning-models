450fd14525304a5c09f0918668a51c17fdc6b0b3
Mubashir Ali <mubaxhiralley@gmail.com>
2026-09-16 20:50:41 +0500

    Add decision tree imports

---
+++ Decision-Tree/decision_tree_heart_disease.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+import pandas as pd
+
+import matplotlib.pyplot as plt
+
+import seaborn as sns
+
+from sklearn.model_selection import train_test_split, cross_val_score
+
+from sklearn.tree import DecisionTreeClassifier, plot_tree
+
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
