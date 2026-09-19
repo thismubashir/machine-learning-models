80bca15e514c95bf5418bd486d509060b17a679b
Mubashir Ali <mubaxhiralley@gmail.com>
2026-09-19 16:15:08 +0500

    Add random forest imports

---
+++ Random-Forest/random_forest_blood_cancer.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+import matplotlib.pyplot as plt
+
+from sklearn.model_selection import train_test_split
+
+from sklearn.preprocessing import LabelEncoder
+
+from sklearn.ensemble import RandomForestClassifier
+
+from sklearn.metrics import (
+
+    accuracy_score,
+
+    confusion_matrix,
+
+    classification_report
+
+)
