0734a85ad815075123cda4a41dc7159d121ce523
Mubashir Ali <mubaxhiralley@gmail.com>
2026-09-19 16:22:43 +0500

    Add random forest train test split

--- Random-Forest/random_forest_blood_cancer.py
+++ Random-Forest/random_forest_blood_cancer.py
@@ -40,3 +40,17 @@ df["gender"] = le.fit_transform(df["gender"])
 X = df.drop("diagnosis", axis=1)
 
 y = df["diagnosis"]
+
+X_train, X_test, y_train, y_test = train_test_split(
+
+    X,
+
+    y,
+
+    test_size=0.2,
+
+    random_state=42,
+
+    stratify=y
+
+)
