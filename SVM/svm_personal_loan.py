d3272da565c79d87918cbc1f0eaa5a0f99332b8e
Mubashir Ali <mubaxhiralley@gmail.com>
2026-09-21 18:57:02 +0500

    Add SVM imports

---
+++ SVM/svm_personal_loan.py
@@ -0,0 +1,27 @@
+import pandas as pd
+
+import matplotlib.pyplot as plt
+
+import seaborn as sns
+
+from sklearn.model_selection import train_test_split
+
+from sklearn.preprocessing import StandardScaler
+
+from sklearn.svm import SVC
+
+from sklearn.metrics import (
+
+    accuracy_score,
+
+    precision_score,
+
+    recall_score,
+
+    f1_score,
+
+    confusion_matrix,
+
+    classification_report
+
+)
