139c1c0d91c5518216b8b9743a4b899eda094e16
Mubashir Ali <mubaxhiralley@gmail.com>
2026-09-22 18:33:30 +0500

    Add SVM feature scaling

--- SVM/svm_personal_loan.py
+++ SVM/svm_personal_loan.py
@@ -171,3 +171,8 @@ X_train, X_test, y_train, y_test = train_test_split(
     stratify=y
 
 )
+scaler = StandardScaler()
+
+X_train_scaled = scaler.fit_transform(X_train)
+
+X_test_scaled = scaler.transform(X_test)
