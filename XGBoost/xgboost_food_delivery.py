e06216781f55a8c4ef5694f217777b335cd49ca3
Mubashir Ali <mubaxhiralley@gmail.com>
2026-09-23 19:12:54 +0500

    Add food delivery dataset loading and understanding

--- XGBoost/xgboost_food_delivery.py
+++ XGBoost/xgboost_food_delivery.py
@@ -8,3 +8,17 @@ from sklearn.model_selection import train_test_split, RandomizedSearchCV
 from sklearn.metrics import mean_absolute_error, r2_score
 
 from xgboost import XGBRegressor
+# 2. Load Dataset
+
+df = pd.read_csv("/content/Food_Delivery_Times.csv")
+
+print(df.head())
+print("\nDataset Shape:")
+print(df.shape)
+
+print("\nDataset Information:")
+print(df.info())
+
+print("\nMissing Values:")
+print(df.isnull().sum())
+target = "Delivery_Time_min"
